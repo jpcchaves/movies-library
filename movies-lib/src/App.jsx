c29eacd4fb9a61717009582627cff19d8ce54ed0
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-06 23:21:21 -0300

    created MovieCard component, updated Home

--- movies-lib/src/App.jsx
+++ movies-lib/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>movies lib</h2>
       <Outlet />
     </div>
   );
--- movies-lib/src/pages/Home.jsx
+++ movies-lib/src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [topMovies, setTopMovies] = useState([]);
 
-export default Home
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Filmes mais bem avaliados:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
