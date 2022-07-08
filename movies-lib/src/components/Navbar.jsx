98075c6b4dcd6ce7c677b41cf6729240034c2bf8
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-07 22:26:52 -0300

    navbar CSS created

--- movies-lib/src/components/Navbar.jsx
+++ movies-lib/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
+// CSS
+import "./Navbar.css";
+
 const Navbar = () => {
   return (
     <nav id="navbar">
       <h2>
         <Link to="/">
           <BiCameraMovie />
-           MoviesLib
+          MoviesLib
         </Link>
       </h2>
       <h2>
@@ -16,7 +19,9 @@ const Navbar = () => {
       </h2>
       <form>
         <input type="text" placeholder="Busque um filme" />
-        <button type="submit"><BiSearchAlt2/></button>
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
       </form>
     </nav>
   );
